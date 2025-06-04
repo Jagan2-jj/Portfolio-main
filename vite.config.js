4bdfdb1b42339451e56dab200c3c4775daa505e6
Jagan2-jj <jk8523301@gmail.com>
2025-06-04 14:07:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -184,6 +184,7 @@ logger.error = (msg, options) => {
 export default defineConfig({
 	customLogger: logger,
 	plugins: [react(), addTransformIndexHtml],
+	base:"/Portfolio-main"
 	server: {
 		cors: true,
 		headers: {
