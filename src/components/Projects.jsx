312fc421a8418f02b71354f03500d90a9b102787
Jagan2-jj <jk8523301@gmail.com>
2025-06-04 17:44:27 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -32,7 +32,7 @@ const projects = [
     description: "A visually stunning portfolio website for a digital artist, featuring interactive galleries and smooth animations.",
     tags: ["React", "Three.js", "GSAP", "Netlify"],
     imageAlt: "Creative portfolio website with interactive 3D elements and image galleries",
-    imageSrc: "/p2.jpg", // Add this line
+    imageSrc: "https://raw.githubusercontent.com/Jagan2-jj/Portfolio-main/refs/heads/main/p2.jpg", // Add this line
     liveUrl: "https://example.com/project3",
     githubUrl: "https://github.com/username/project3"
   },
